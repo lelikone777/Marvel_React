dbec5961e9c79a54f273dd4311ca2addd87c0371
lelikone777 <2257855@gmail.com>
2023-02-02 02:12:05 +0300

    list

--- .idea/src/components/app/App.js
+++ .idea/src/components/app/App.js
@@ -1,7 +1,9 @@
+import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 function App() {
   return (
     <div className="App">
+        <AppHeader/>
         <RandomChar/>
     </div>
   );
