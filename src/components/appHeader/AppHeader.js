d709fdf5eb9c22712cdd5139e7e102268f007a07
lelikone777 <2257855@gmail.com>
2023-02-03 00:01:43 +0300

    error boundery

---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,25 @@
+import './appHeader.scss';
+
+const AppHeader = () => {
+    return (
+        <header className="app__header">
+            <h1 className="app__title">
+                <a href="https://www.google.com/">
+                    <span>Marvel</span> information portal
+                </a>
+            </h1>
+            <nav className="app__menu">
+                <ul>
+                    <li>
+                        <a href="https://www.google.com/">Characters</a>
+                    </li>
+                    <li>
+                        <a href="https://www.google.com/">Comics</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
